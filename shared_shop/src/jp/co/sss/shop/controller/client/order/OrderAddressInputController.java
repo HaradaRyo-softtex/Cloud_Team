f4f17509a59ad255a2b496b7c76bd8e0ba9a2aa7
kosaki <k.kosaki@softtex.co.jp>
2025-05-23 13:39:34 +0900

    入力画面のサーブレット

--- shared_shop/src/jp/co/sss/shop/controller/client/order/OrderAddressInputController.java
+++ shared_shop/src/jp/co/sss/shop/controller/client/order/OrderAddressInputController.java
@@ -2,6 +2,7 @@ package jp.co.sss.shop.controller.client.order;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,23 +13,36 @@ import javax.servlet.http.HttpSession;
 
 import jp.co.sss.shop.bean.UserBean;
 import jp.co.sss.shop.bean.UserDetailBean;
+import jp.co.sss.shop.constant.Constant;
+import jp.co.sss.shop.constant.URLConstant;
 import jp.co.sss.shop.dao.UserDao;
+import jp.co.sss.shop.form.OrderForm;
+import jp.co.sss.shop.validator.UserInputValid;
 
 /**
  * Servlet implementation class OrderAddressInputController
  */
-@WebServlet("/order/address/input ")
+@WebServlet("/order/address/input")
 public class OrderAddressInputController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-        
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String backflg=request.getParameter("backflg");
-		if(backflg==null) {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String backflg = request.getParameter("backflg");
+		if (backflg == null) {
 			HttpSession session = request.getSession();
-			UserBean user_bean =  (UserBean) session.getAttribute("user");
+			UserBean user_bean = (UserBean) session.getAttribute("user");
 			int user_id = user_bean.getId();
 			try {
-				UserDetailBean bean= UserDao.findOneByUserId(user_id);
+				UserDetailBean userdetailbean = UserDao.findOneByUserId(user_id);
+				OrderForm orderform=new OrderForm();
+				orderform.setPostalCode(userdetailbean.getPostalCode());
+				orderform.setAddress(userdetailbean.getAddress());
+				orderform.setName(userdetailbean.getName());
+				orderform.setPhoneNumber(userdetailbean.getPhoneNumber());
+				
+				session.setAttribute("orderform",orderform);
+				request.getRequestDispatcher("/jsp/client/order/input_address.jsp").forward(request, response);
 			} catch (ClassNotFoundException e) {
 				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
@@ -36,8 +50,53 @@ public class OrderAddressInputController extends HttpServlet {
 				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
 			}
+
+		} else if (backflg.equals("off")) {
+			HttpSession session = request.getSession();
+			OrderForm orderform = new OrderForm();
+			orderform = (OrderForm) session.getAttribute("orderform");
+
+			orderform.setPostalCode(request.getParameter("postalCode"));
+			orderform.setAddress(request.getParameter("address"));
+			orderform.setName(request.getParameter("name"));
+			orderform.setPhoneNumber(request.getParameter("phoneNumber"));
+
+			//エラーメッセージ
+			List<String> errorMessageList = null;
+			try {
+				errorMessageList = UserInputValid.orderInputErrorMessageList(orderform);
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+				System.out.println(7);
+				response.sendRedirect(request.getContextPath() + URLConstant.URL_ERROR_TYPE + Constant.ERROR_CODE_DB);
+				return;
+			}
+			if (errorMessageList.size() > 0) {
+
+				// 入力値にエラーがある場合
+				// リクエストスコープにセット
+				request.setAttribute("orderform", orderform);
+				request.setAttribute("errorMessageList", errorMessageList);
+
+				// 会員登録入力画面を表示
+				request.getRequestDispatcher("/jsp/client/order/input_address.jsp").forward(request, response);
+
+			} else {
+				// 入力値にエラーがない場合
+				// セッションスコープにセット
+				session.setAttribute("orderform", orderform);
+				response.sendRedirect(request.getContextPath() + "/order/payment/input");
+			}
 			
+
+		} else if(backflg.equals("on")){
+			request.getRequestDispatcher("/jsp/client/order/input_address.jsp").forward(request, response);
+		
+		}else {
+			// backflgが存在するが、値が想定外(Hidden情報が書き換えられた)
+			response.sendRedirect(
+					request.getContextPath() + URLConstant.URL_ERROR_TYPE + Constant.ERROR_CODE_UNEXPECTED_TRANSITION);
 		}
 	}
- 
+
 }
